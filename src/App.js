c2a5eb92281e0d570fd50c8a867d9ca548a226ea
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-13 09:35:52 +0300

    redux structura

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { compose } from "redux";
+import { compose, createStore } from "redux";
+import redux from "./redux/redux";
 
+const initialState = [
+  {
+    name: "doston",
+    age: "20",
+  },
+];
 function App() {
   const [state, setState] = useState("");
+  const dispatch = useDispatch();
   useEffect(() => {
-    setState(natija);
-  }, [state]);
-  const kup = (num) => {
-    return num * 2;
+    console.log(initialState);
+  });
+
+  const personAction = (name, age) => {
+    return {
+      type: "ADD",
+      payload: {
+        name: name,
+        age: age,
+      },
+    };
   };
-  const ayir = (num) => {
-    return num - 2;
+
+  const personReducers = (state = initialState, action) => {
+    if (action.type === "ADD") {
+      return [...state, action.payload];
+    }
+    return state;
   };
-  const qush = (num) => {
-    return num + 2;
-  };
-  const mixFunc = compose(qush, ayir, kup);
-  const natija = mixFunc(30);
-  console.log(natija);
+  const newObj = personAction("oybek", 11);
+  const store = createStore(personReducers);
+
 
   return (
     <div className="App">
-      <h1>Doston</h1>
-      <h1>Natija:{state}</h1>
+      {/* <h1>Doston</h1>
+      <h1>Natija:{state}</h1> */}
     </div>
   );
 }
---
+++ src/redux/redux.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const redux = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+
+export default redux;
