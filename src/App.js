9a3b6601449602261e619ba5140adc4aaf31c086
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-13 14:54:41 +0300

    ConfigureStore

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { compose, createStore } from "redux";
-import redux from "./redux/redux";
+import React, { useEffect } from "react";
 
-const initialState = [
-  {
-    name: "doston",
-    age: "20",
-  },
-];
-function App() {
-  const [state, setState] = useState("");
-  const dispatch = useDispatch();
-  useEffect(() => {
-    console.log(initialState);
-  });
-
-  const personAction = (name, age) => {
-    return {
-      type: "ADD",
-      payload: {
-        name: name,
-        age: age,
-      },
-    };
-  };
-
-  const personReducers = (state = initialState, action) => {
-    if (action.type === "ADD") {
-      return [...state, action.payload];
-    }
-    return state;
-  };
-  const newObj = personAction("oybek", 11);
-  const store = createStore(personReducers);
-
-
-  return (
-    <div className="App">
-      {/* <h1>Doston</h1>
-      <h1>Natija:{state}</h1> */}
-    </div>
-  );
-}
+const App = () => {
+  return <div className="app"></div>;
+};
 
 export default App;
