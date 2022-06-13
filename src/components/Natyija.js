8a3b35c9835f85b489b007ac67ce0a18bf530233
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-13 15:50:22 +0300

    render ui

--- src/components/Natyija.js
+++ src/components/Natyija.js
@@ -1,10 +1,40 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-
+import { increment, decrement } from "../redux/Counter";
+import { useDispatch } from "react-redux";
 const Natyija = () => {
-  const store = useSelector((store) => store);
+  const dispatch = useDispatch();
 
-  return <div className="natija"></div>;
+  const store = useSelector((store) => store);
+  return (
+    <div className="natija">
+      <h1>{store.counter.data}</h1>
+      <button
+        style={{
+          width: "100px",
+          height: "50px",
+          border: "none",
+          color: "white",
+          background: "crimson",
+        }}
+        onClick={() => dispatch(increment())}
+      >
+        +
+      </button>
+      <button
+        style={{
+          width: "100px",
+          height: "50px",
+          border: "none",
+          color: "white",
+          background: "blue",
+        }}
+        onClick={() => dispatch(decrement())}
+      >
+        -
+      </button>
+    </div>
+  );
 };
 
 export default Natyija;
