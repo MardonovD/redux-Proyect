e0d3c9684d59bd6b0f9f31438b1a490e9caafbfe
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-13 15:32:52 +0300

    action create fies

--- src/components/Natyija.js
+++ src/components/Natyija.js
@@ -2,10 +2,6 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const Natyija = () => {
-  useEffect(() => {
-    console.log(store);
-  });
-  
   const store = useSelector((store) => store);
 
   return <div className="natija"></div>;
